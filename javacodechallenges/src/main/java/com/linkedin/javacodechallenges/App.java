f9999e8ff06ba14851b074adb868b375be17ef00
atirake <54449819+atirake@users.noreply.github.com>
2022-12-01 09:14:24 +0000

    testing

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,11 +1,51 @@
 package com.linkedin.javacodechallenges;
 
+import java.net.PasswordAuthentication;
 import java.util.Scanner;
 
 public class App {
 
     public static boolean isPasswordComplex(String password) {
-        return false;
+        if (password.length() < 6) {
+            return false;
+        } else {
+            boolean upperCheck = false;
+            for (char ch : password.toCharArray()) {
+                if (ch >= 65 && ch <= 90) {
+                    upperCheck = true;
+                    break;
+                }
+            }
+            if (!upperCheck) {
+                return false;
+            } else {
+                boolean lowerCheck = false;
+                for (char ch : password.toCharArray()) {
+                    if (ch >= 97 && ch <= 122) {
+                        lowerCheck = true;
+                        break;
+                    }
+                }
+                if (!lowerCheck) {
+                    return false;
+                }
+                else {
+                    boolean numberCheck = false;
+                    for (char ch : password.toCharArray()) {
+                        if (ch >= 48 && ch <= 57) {
+                            numberCheck = true;
+                            break;
+                        }
+                    }
+                    if (!numberCheck) {
+                        return false;
+                    }
+                    else {
+                        return true;
+                    }
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
